f9a05cba4c6e80534c1d432591c9c8e7c01e4d51
Hu, Smile <smile.hu@sap.com>
2017-09-04 22:52:01 +0800

    Init the project.

---
+++ TIKU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TIKU
+//
+//  Created by Hu, Smile on 2017/9/4.
+//  Copyright © 2017年 Hu, Smile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TIKU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TIKU
+//
+//  Created by Hu, Smile on 2017/9/4.
+//  Copyright © 2017年 Hu, Smile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
